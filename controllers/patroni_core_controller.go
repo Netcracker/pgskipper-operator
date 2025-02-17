b31f2397ea60f410fe020ad9af9c8381ba3410be
anshajsingh <42251874+anshajsingh@users.noreply.github.com>
2025-02-17 11:13:53 +0530

    fix pgbackrest after major upgrade

--- controllers/patroni_core_controller.go
+++ controllers/patroni_core_controller.go
@@ -55,6 +55,8 @@ import (
 var (
 	MasterLabel                   = map[string]string{"pgtype": "master"}
 	patroniCoreOperatorLockCmName = "patroni-core-operator-lock"
+	backRestcontainerName         = "pgbackrest-sidecar"
+	stanzaUpgradeCommand          = "pgbackrest stanza-upgrade"
 	//pgHost                          = util.GetEnv("POSTGRES_HOST", "pg-patroni")
 )
 
@@ -311,7 +313,7 @@ func (pr *PatroniCoreReconciler) Reconcile(ctx context.Context, request ctrl.Req
 		return reconcile.Result{RequeueAfter: time.Minute}, err
 	}
 
-	if cr.Spec.PgBackRest != nil {
+	if cr.Spec.PgBackRest != nil && cr.Spec.Patroni != nil {
 		masterPod, err := pr.helper.ResourceManager.GetPodsByLabel(MasterLabel)
 		if err != nil || len(masterPod.Items) == 0 {
 			pr.logger.Error("Can't get Patroni Leader for stanza upgrade execution", zap.Error(err))
@@ -319,10 +321,8 @@ func (pr *PatroniCoreReconciler) Reconcile(ctx context.Context, request ctrl.Req
 		}
 		masterPodName := masterPod.Items[0].Name
 		namespace := util.GetNameSpace()
-		backRestcontainer := "pgbackrest-sidecar"
-		backRestcommand := "pgbackrest stanza-upgrade"
 		pr.logger.Info("executing command to upgrade pgBackRest stanza")
-		stdout, stderr, err := pr.helper.ExecCmdOnPod(masterPodName, namespace, backRestcontainer, backRestcommand)
+		stdout, stderr, err := pr.helper.ExecCmdOnPod(masterPodName, namespace, backRestcontainerName, stanzaUpgradeCommand)
 		if err != nil {
 			fmt.Printf("Failed to execute stanza-upgrade command: %v\nStderr: %s\n", err, stderr)
 		} else {
