4b8374e4407ffbded9b1ae080b13becb34aa06fb
yerkennz <yerken1656@gmail.com>
2026-05-22 19:16:50 +0500

    fix: check

--- tests/docker/robot-entrypoint.sh
+++ tests/docker/robot-entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
 export TAGS="${TESTS_TAGS:-$TAGS}"
 cd "${ROBOT_HOME:-/opt/robot}" || exit 1
-
-/docker-entrypoint.sh "$@"
-robot_exit=$?
-
-echo "Robot finished with exit code ${robot_exit}. Waiting before pod exit to allow result copy..."
-sleep "${RESULT_COPY_GRACE_PERIOD:-300}"
-
-exit "${robot_exit}"
+exec /docker-entrypoint.sh "$@"
