fe561ff7639c695ce1bfc27cfc5276e5d736f0ba
yerkennz <yerken1656@gmail.com>
2026-05-22 17:35:06 +0500

    fix: increase sleep time

--- tests/docker/robot-entrypoint.sh
+++ tests/docker/robot-entrypoint.sh
@@ -6,6 +6,7 @@ cd "${ROBOT_HOME:-/opt/robot}" || exit 1
 /docker-entrypoint.sh "$@"
 robot_exit=$?
 
-sleep "${RESULT_COPY_GRACE_PERIOD:-60}"
+echo "Robot finished with exit code ${robot_exit}. Waiting for result copy..."
+sleep "${RESULT_COPY_GRACE_PERIOD:-120}"
 
-exit "$robot_exit"
+exit "${robot_exit}"
