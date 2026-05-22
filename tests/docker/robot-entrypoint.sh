0d58be620176d8f48e5f427aca243f0d8d6589c1
yerkennz <yerken1656@gmail.com>
2026-05-22 18:23:02 +0500

    fix: increase wait time

--- tests/docker/robot-entrypoint.sh
+++ tests/docker/robot-entrypoint.sh
@@ -6,7 +6,7 @@ cd "${ROBOT_HOME:-/opt/robot}" || exit 1
 /docker-entrypoint.sh "$@"
 robot_exit=$?
 
-echo "Robot finished with exit code ${robot_exit}. Waiting for result copy..."
-sleep "${RESULT_COPY_GRACE_PERIOD:-120}"
+echo "Robot finished with exit code ${robot_exit}. Waiting before pod exit to allow result copy..."
+sleep "${RESULT_COPY_GRACE_PERIOD:-300}"
 
 exit "${robot_exit}"
