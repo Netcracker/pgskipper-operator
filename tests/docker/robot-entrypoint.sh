6dfd7df6dbcb7e9a5a9a8eba89c11192df888b68
yerkennz <yerken1656@gmail.com>
2026-05-22 16:59:04 +0500

    fix: add time  for copying reports

--- tests/docker/robot-entrypoint.sh
+++ tests/docker/robot-entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
 export TAGS="${TESTS_TAGS:-$TAGS}"
 cd "${ROBOT_HOME:-/opt/robot}" || exit 1
-exec /docker-entrypoint.sh "$@"
+
+/docker-entrypoint.sh "$@"
+robot_exit=$?
+
+sleep "${RESULT_COPY_GRACE_PERIOD:-60}"
+
+exit "$robot_exit"
