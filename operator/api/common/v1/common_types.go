22e518cd98db563b9a4a3832e025c11c144858ee
yerkennz <yerken1656@gmail.com>
2026-08-04 18:55:21 +0500

    feat: [CPCAP-12366] add custom annotation for PVC in helm charts

--- operator/api/common/v1/common_types.go
+++ operator/api/common/v1/common_types.go
@@ -20,13 +20,14 @@ package v1
 // Storage Describes Storage that will be used by patroni
 type Storage struct {
 	// +kubebuilder:validation:Pattern=`^[0-9]+(m|Ki|Mi|Gi|Ti|Pi|Ei|k|M|G|T|P|E)$`
-	Size         string   `json:"size,omitempty"`
-	Type         string   `json:"type,omitempty"`
-	StorageClass string   `json:"storageClass,omitempty"`
-	Volumes      []string `json:"volumes,omitempty"`
-	Nodes        []string `json:"nodes,omitempty"`
-	Selectors    []string `json:"selectors,omitempty"`
-	AccessModes  []string `json:"accessModes,omitempty"`
+	Size         string            `json:"size,omitempty"`
+	Type         string            `json:"type,omitempty"`
+	StorageClass string            `json:"storageClass,omitempty"`
+	Volumes      []string          `json:"volumes,omitempty"`
+	Nodes        []string          `json:"nodes,omitempty"`
+	Selectors    []string          `json:"selectors,omitempty"`
+	AccessModes  []string          `json:"accessModes,omitempty"`
+	Annotations  map[string]string `json:"annotations,omitempty"`
 }
 type CloudSql struct {
 	Project        string `json:"project,omitempty"`
