06deeb2d994167931183978d8573ff551058d167
svetychkina <svetych494@gmail.com>
2026-09-02 15:05:48 +0500

    fix: [PSUPCLCAP-5487] delete duplication

--- operator/pkg/deployment/patroni.go
+++ operator/pkg/deployment/patroni.go
@@ -362,15 +362,6 @@ func NewPatroniStatefulset(cr *patroniv1.PatroniCore, deploymentIdx int, cluster
 			},
 		})
 	}
-
-	patroniLogLevel := patroniSpec.PatroniLogLevel
-	if patroniLogLevel == "" {
-		patroniLogLevel = "WARNING"
-	}
-	stSet.Spec.Template.Spec.Containers[0].Env = append(stSet.Spec.Template.Spec.Containers[0].Env, corev1.EnvVar{
-		Name:  "PATRONI_LOG_LEVEL",
-		Value: patroniLogLevel,
-	})
 	
 	// TLS Section
 	if cr.Spec.Tls != nil && cr.Spec.Tls.Enabled {
