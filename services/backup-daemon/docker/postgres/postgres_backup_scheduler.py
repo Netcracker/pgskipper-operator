895097c1e38675dcb746a3b9dca1ddde87782efc
Tvion <44255078+Tvion@users.noreply.github.com>
2026-09-10 12:50:26 +0500

    fix: [PSUPCLCAP-5072] Backup timeout handling (#612)

--- services/backup-daemon/docker/postgres/postgres_backup_scheduler.py
+++ services/backup-daemon/docker/postgres/postgres_backup_scheduler.py
@@ -109,30 +109,34 @@ class BackupsScheduler:
                                                      backup_id=backup.get_backup_id()
                                                      )
 
-                timeout_seconds = self.__backup_options.get('timeout')
-                if timeout_seconds:
-                    self.__log.info("Using configured timeout for backup: %ss", timeout_seconds)
-                    worker.join(timeout_seconds)
-                else:
-                    if oldest_backup:
-                        self.__log.info("Id of latest backup: {}".format(oldest_backup.get_id()))
-                        spent_time = oldest_backup.load_metrics().get('spent_time')
+                backup_timeout = None
+                timeout_from_options = self.__backup_options.get('timeout')
+                if timeout_from_options:
+                    backup_timeout = timeout_from_options                    
 
-                        if spent_time:
-                            # time stored as milliseconds converting to seconds and double the value
-                            time_out = spent_time / 1000 * 2
-                            self.__log.info("Setting timeout for backup process: {}".format(time_out))
-                            worker.join(time_out)
+                if oldest_backup:
+                    self.__log.info("Id of latest backup: {}".format(oldest_backup.get_id()))
+                    spent_time = oldest_backup.load_metrics().get('spent_time')
 
-                        else:
-                            worker.join()
-                    else:
-                        worker.join()
+                    if spent_time:
+                        # time stored as milliseconds converting to seconds and double the value
+                        calculated_timeout = spent_time / 1000 * 2
+                        if backup_timeout is None or calculated_timeout > backup_timeout:
+                            backup_timeout = calculated_timeout
+                            self.__log.info("Setting calculated timeout for backup process: %ss", backup_timeout)
+                        elif backup_timeout is not None:
+                            self.__log.info("Setting configured timeout for backup process: %ss", backup_timeout)
+
+                worker.join(backup_timeout)
 
                 self.__log.info("Worker completed: {}".format(not worker.is_alive()))
 
                 if worker.is_alive():
-                    self.__log.error("Backup worker for {} is not completed after timeout: {}".format(backup.get_backup_id(), time_out))
+                    self.__log.error(
+                        "Backup worker for %s did not complete within the %ss timeout",
+                        backup.get_backup_id(),
+                        backup_timeout,
+                    )
                     worker.fail()
                     worker.kill()
                     raise Exception("Backup worker timeout exceeded")
